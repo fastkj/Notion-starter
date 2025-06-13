ec41049b7245e33836c4aec784325cf8404f99e3
fastkj <96054223+fastkj@users.noreply.github.com>
2025-06-13 22:12:10 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,7 +12,7 @@ import * as React from 'react'
 import * as config from '@/lib/config'
 import { useDarkMode } from '@/lib/use-dark-mode'
 
-// import styles from './styles.module.css'
+import styles from './styles.module.css'
 
 // TODO: merge the data and icons from PageSocial with the social links in Footer
 
